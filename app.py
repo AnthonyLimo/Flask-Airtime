10fa287d3892cc70975ecb2320c241d28667a547
Anthony Kiplimo <anthonylimo90@gmail.com>
2019-01-21 10:52:49 +0300

    Updates files and core logic

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import africastalking
+
+app = Flask(__name__)
+
+username = "sandbox"
+api_key = ""
+
+africastalking.initialize(username, api_key)
+
+@app.route("/")
+def index():
+	if request.method == "POST":
+		airtime_amount = request.form[]
+		phone_number = request.form[]
+
+		print(airtime_amount)
+		print(phone_number)
+
+		response = airtime.send(airtime_amount, [phone_number])
+
+	return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
