6ac34909f6faed560ed38578a7ffd39283e9794b
Anthony Kiplimo <anthonylimo90@gmail.com>
2019-01-22 12:21:53 +0300

    Update project and bug fix
    Updated index.html to with better placeholders
    Update base.html with better structure
    Fixed broken app.py. Now able to send airtime

--- app.py
+++ app.py
@@ -5,22 +5,33 @@ import africastalking
 app = Flask(__name__)
 
 username = "sandbox"
-api_key = ""
+api_key = "ENTER_API_KEY_HERE"
 
 africastalking.initialize(username, api_key)
 
-@app.route("/", methods=["POST"])
+airtime = africastalking.Airtime
+
+@app.route("/", methods=["POST","GET"])
 def index():
 	if request.method == "POST":
-		airtime_amount = request.form[]
-		phone_number = request.form[]
+		airtime_amount = request.form["airtimeAmount"]
+		phone_number = request.form["phoneNumber"]
+		country_code = "KES"
 
-		print(airtime_amount)
-		print(phone_number)
+		recipients = [{
+			"phoneNumber": phone_number,
+			"amount": airtime_amount,
+			"currency_code": "KES"  
+		}]
+		
 
-		response = airtime.send(airtime_amount, [phone_number])
+		response = airtime.send(recipients=recipients)
+
+		print(response)
+
+
 
 	return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(port=os.environ.get('PORT'))
+    app.run(port = os.environ.get('PORT'))
